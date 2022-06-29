ff50e9cc1e59533266c319e0cc2f76ab15f04943
ugojnr <danielugochukwuofomuogbo@gmail.com>
2022-06-30 00:42:43 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <sidebar />
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Sidebar from "./components/Sidebar";
+
+export default {
+  name: "App",
+
+  components: {
+    Sidebar,
+  },
+
+  data: () => ({}),
+};
+</script>
